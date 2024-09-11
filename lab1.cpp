0dbfde113c739374495620918243b1aefe0154d3
VrejHambardzumyan <vrezhambardzumyan04@gmail.com>
2024-09-11 14:13:34 +0400

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,32 @@
+#include <iostream.h>
+
+using namespace std;
+
+int yndhanurBajanarar(int a, int b)
+{
+   
+
+    while (a != b)
+    {
+       while (a < b)
+        {   
+            b = b - a;
+        }
+        while (a > b)
+        {
+            a = a - b;
+        }
+
+    }
+    cout>>"the number is\n"
+    cout>>a;
+    
+}
+
+
+
+int main()
+{
+    yndhanurBajanarar(64,)
+
+}
